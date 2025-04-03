46ceed0d29236add3068966cbb384a8b0b812f86
iguinho-ux <igormachadosr@gmail.com>
2025-04-03 11:32:40 -0300

    ex6

---
+++ ex5/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        // Solicita ao usuário um número
+        Console.Write("Digite um número: ");
+        double numero = double.Parse(Console.ReadLine());
+
+        // Verifica se o número é positivo, negativo ou zero
+        if (numero > 0)
+        {
+            Console.WriteLine("O número é POSITIVO.");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("O número é NEGATIVO.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ZERO.");
+        }
+    }
+}
---
+++ ex6/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        // Solicita a nota do aluno
+        Console.Write("Digite a nota do aluno: ");
+        double nota = double.Parse(Console.ReadLine());
+
+        // Verifica o conceito do aluno baseado na nota
+        if (nota >= 9)
+        {
+            Console.WriteLine("Conceito: A");
+        }
+        else if (nota >= 7)
+        {
+            Console.WriteLine("Conceito: B");
+        }
+        else if (nota >= 5)
+        {
+            Console.WriteLine("Conceito: C");
+        }
+        else
+        {
+            Console.WriteLine("Conceito: D");
+        }
+    }
+}
