fcd3dd75cf4ff26229fd1af363effd74ba9701e7
iguinho-ux <igormachadosr@gmail.com>
2025-04-03 11:23:00 -0300

    ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        // Solicita ao usuário um número de 1 a 7
+        Console.Write("Digite um número de 1 a 7: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Usa a estrutura switch-case para verificar o dia da semana
+        switch (numero)
+        {
+            case 1:
+                Console.WriteLine("Domingo");
+                break;
+            case 2:
+                Console.WriteLine("Segunda-feira");
+                break;
+            case 3:
+                Console.WriteLine("Terça-feira");
+                break;
+            case 4:
+                Console.WriteLine("Quarta-feira");
+                break;
+            case 5:
+                Console.WriteLine("Quinta-feira");
+                break;
+            case 6:
+                Console.WriteLine("Sexta-feira");
+                break;
+            case 7:
+                Console.WriteLine("Sábado");
+                break;
+            default:
+                Console.WriteLine("Número inválido. Digite um número de 1 a 7.");
+                break;
+        }
+    }
+}
