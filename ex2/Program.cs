41a48b3e1ea684bc56db37fc7eaa68c939dbe96e
iguinho-ux <igormachadosr@gmail.com>
2025-04-03 11:12:29 -0300

    vihhhhhh nv2

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main()
+    {
+        // Solicitar a idade da pessoa
+        Console.WriteLine("Qual sua idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        if (idade < 18)
+
+        {
+            Console.WriteLine("Menor de idade");
+
+        }
+
+        else if (idade >= 18)
+
+        {
+            Console.WriteLine("Maior de Idade");
+        }
+
+    }
+}
