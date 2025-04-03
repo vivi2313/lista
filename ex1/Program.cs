e9731b9fce293eab2b2d5a3c6ab117cfedae5a06
iguinho-ux <igormachadosr@gmail.com>
2025-04-03 11:10:59 -0300

    vih nv

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main()
+    {
+        // Solicita ao usuário um número inteiro
+        Console.Write("Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Ver se o número é par ou ímpar
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número é PAR.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ÍMPAR.");
+        }
+
+
+    }
+}
